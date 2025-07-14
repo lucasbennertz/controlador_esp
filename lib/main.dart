5999eb04f34458ee802dc75e3483f9669baccd88
lucasbennertz <lucasgcbennertz@gmail.com>
2025-07-14 17:59:18 -0300

    deixando mais bonito

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:controle_esp/widgets/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
-  await dotenv.load(fileName: ".env");
+Future<void> main() async{
+  await dotenv.load();
   runApp(const MainScreen());
 }
 
--- lib/widgets/buttons/botao_led.dart
+++ lib/widgets/buttons/botao_led.dart
@@ -5,35 +5,49 @@ import 'package:flutter/material.dart';
 class BotaoLed extends StatefulWidget {
   BotaoLed({super.key, required this.numeroLed});
   final int numeroLed;
-  bool ledLigado = false;
   @override
   State<BotaoLed> createState() => _BotaoLedState();
 }
 
 class _BotaoLedState extends State<BotaoLed> {
+  bool ledLigado = false;
+
+  _ligarLed(int numeroLed){
+    HttpRequest http = HttpRequest();
+    http.ligarLed(numeroLed);
+  }
+  _desligarLed(int numeroLed){
+    HttpRequest http = HttpRequest();
+    http.desligarLed(numeroLed);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
       child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.blue),
+          ),
           onPressed: (){
-            if(widget.ledLigado){
+            if(ledLigado){
               _desligarLed(widget.numeroLed);
-              widget.ledLigado =false;
             }else{
               _ligarLed(widget.numeroLed);
-              widget.ledLigado = true;
             }
+            setState(() {
+              ledLigado = !ledLigado;
+            });
           },
-          child: Text(widget.ledLigado ? "desligar Led ${widget.numeroLed}" : "ligar Led ${widget.numeroLed}")),
+          child: Text(
+              ledLigado ?
+          "desligar Led ${widget.numeroLed}" :
+              "ligar Led ${widget.numeroLed}",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: MediaQuery.of(context).size.width * 0.05
+          ),
+          )),
     );
   }
-}
-_ligarLed(int numeroLed){
-  HttpRequest http = HttpRequest();
-  http.ligarLed(numeroLed);
-}
-_desligarLed(int numeroLed){
-  HttpRequest http = HttpRequest();
-  http.desligarLed(numeroLed);
 }
